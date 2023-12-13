0500fa5311dae87ac51c9fbda28ff3c5455c3c5d
Akalisho <kubapustul7@gmail.com>
2023-12-13 09:43:21 +0100

    13.12.2023

--- programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
+++ programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
@@ -8,7 +8,28 @@ Operatory porównania:
 == równe
 != ró¿ne
 */
+/*
+Priorytety operatorów 
+* /
++ -
+> < >= <= !=
+&&
+||
+/*
+Operatory logiczne:
+A B     A || B      A && B      !A
+f f       f            f         t
+f t       t            f
+t f       t            f         f
+t t       t            t
+*/
 // Napisz program, który  pobierze liczbe od u¿ytkownika i wyœwietli czy jest ona dodatnia czy nie
+int main()
+{
+    //task1();
+    //task2();
+    task3();
+}
 void task1()
 {
     int number; 
@@ -23,7 +44,40 @@ void task1()
         std::cout << "Liczba jest ujemna"; "\n";
     }
 }
-int main()
+void task2()
 {
-    task1();
+    int number;
+    std::cout << "Podaj liczbe, ktora chcesz sprawdzic; "; "\n";
+    std::cin >> number;
+    if (number >= 1)
+    {
+        if (number < 10)
+        {
+            std::cout << "Liczba jest w zakresie";
+        }
+        else
+        {
+            std::cout << "Liczba nie jest w zakresie";
+        }
+    }
+    else
+    {
+        std::cout << "Liczba nie jest w zakresie";     
+    }
+
 }
+void task3()
+{
+    int number;
+    std::cout << "Podaj liczbe, ktora chcesz sprawdzic; "; "\n";
+    std::cin >> number;
+    if (number >= 1 && number < 10)
+    {
+        std::cout << "Liczba jest w zakresie";
+    }
+    else
+    {
+        std::cout << "Liczba nie jest w zakresie";
+    }
+}
+
