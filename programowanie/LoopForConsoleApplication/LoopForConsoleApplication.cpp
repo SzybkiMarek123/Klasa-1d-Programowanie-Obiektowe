9929cf0a420d3171c18e9e3e43150d66084e520d
Akalisho <kubapustul7@gmail.com>
2024-03-15 14:46:58 +0100

    15.03.2024

--- programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
+++ programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
@@ -16,9 +16,129 @@ void task1()
 		std::cout << "Hello World\n";
 	}
 }
+//Napisz program na ekranie kolejne liczby naturalne od 1 do 10
+void task2()
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		std::cout << i << "\n";
+	}
+}
+//Namaluj równoległobok z "X". Wysokość i szeokość wczytaj z klawiatury.
+void task3()
+{
+	int width, height;
 
+	do
+	{
+		std::cout << "Podaj szerokość: \n";
+		std::cin >> width;
+	} while (width < 0);
+
+	do
+	{
+		std::cout << "Podaj wysokość: \n";
+		std::cin >> height;
+	} while (height < 0);
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			std::cout << "X";
+		}
+		std::cout << "\n";
+	}
+}
+
+/*
+* Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.)
+* Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
+* Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
+* Program obliczający sumę kwadratów liczb od 1 do 10
+* Program, który wczyta podstawę oraz wykładnik oraz wyliczy wynik.
+* Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu (ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
+
+* Program, kóry wyświetli poniższe wzory:
+	****     *       54321        121212        122333
+	***     ***      65432        212121        223334444
+	**     *****     76543        121212        333444455555
+	*     *******    87654        212121        444455555666666
+
+* Oblicz sumę szeregu 1/1^2 + 1/2^2 + 1/3^2 + ... +1/n^2. Jeżeli się nie pomylisz, to dla odpowiednio dużej wartości n po przemnożeniu jej przez 6 i spierwiastkowaniu powinieneś otrzymać wartość liczby π (suma szeregu jest równa π2/6). Zwróć uwagę, że pierwsza wartość indeksu to 1, a nie 0. Dokładność obliczeń można sprawdzić porównując z wartością odczytaną z własności Math.PI.
+
+* Napisz program, który rozkłoży liczbę na czynniki pierwsze.
+
+*/
+//* Program obliczający sumę liczb od 1 do 100
+void task4() 
+{
+	int suma = 0, number;
+
+	for (int i = 1; i <= 100; i++)
+	{
+		suma += i;
+	}
+
+	std::cout << "Suma liczb od 1 do 100 wynosi: " << suma;
+}
+//* Program sprawdzający czy podana liczba jest liczbą doskonałą (czyli taką, której suma dzielników (z wyłączeniem samej siebie) jest równa danej liczbie, np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
+void task5() 
+{
+	int sum = 0, number;
+
+	do
+	{
+		std::cout << "Podaj liczbę: \n";
+		std::cin >> number;
+	} while (number < 0);
+}
+//*Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.)
+void task6() 
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		std::cout << i * i << ", ";
+	}
+}
+//* Program obliczający n!.
+void task7()
+{
+	int number, silnia = 1;
+
+	do
+	{
+		std::cout << "Podaj liczbę dodatnią: ";
+		std::cin >> number;
+	} while (number < 0);
+
+	for (int i = number; i > 1; i--)
+	{
+		silnia *= i;
+	}
+
+	std::cout << "Silnia z liczby " << number << " wynosi: " << silnia;
+}
+//* Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.)
+void task8()
+{
+	int number, silnia = 1;
+
+	do
+	{
+		std::cout << "Podaj liczbę dodatnią: ";
+		std::cin >> number;
+	} while (number < 0);  
+}
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
-	task1();
+	//task1();
+	//task2();
+	//task3();
+	//task4();
+	//task5();
+	//task6();
+	//task7();
+	task8();
 }
