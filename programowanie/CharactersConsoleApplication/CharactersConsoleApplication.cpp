2d8123a56cc5f3e34c03d54a52f3502293b92627
Akalisho <kubapustul7@gmail.com>
2024-04-08 09:32:37 +0200

    08.04.2024

---
+++ programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
@@ -0,0 +1,229 @@
+﻿#include <iostream>;
+
+//Napisz program który pobierze znak od u¿ytkownika i wyœwieli go
+void task1()
+{
+	char characterFromUser;
+	std::cout << "Podaj jeden znak:\n";
+	std::cin >> characterFromUser;
+
+	std::cout << "Podano: " << characterFromUser << "\n";
+
+	characterFromUser = characterFromUser + 1;
+
+	std::cout << "Podano: " << characterFromUser << "\n";
+
+	characterFromUser = 97;
+	characterFromUser = 'a';
+	characterFromUser = 'a' + 1; // 'b'
+}
+
+// Napisz program, który wczyta znak z klawiatury i sprawdzi czy jest to mała litera alfabetu.
+void task2()
+{
+	char characterFromUser;
+	std::cout << "Podaj jeden znak:\n";
+	std::cin >> characterFromUser;
+
+	//wersja 1
+	if (characterFromUser == 'a'
+		|| characterFromUser == 'b'
+		|| characterFromUser == 'c'
+		|| characterFromUser == 'd'
+		//...
+		|| characterFromUser == 'z')
+	{
+		std::cout << "Podałes małą literę alfabetu\n";
+	} else
+	{
+		std::cout << "Podaj inny znak\n";
+	}
+
+	//wersja 2
+	if (characterFromUser >= 97
+		&& characterFromUser <= 122)
+	{
+		std::cout << "Podałes małą literę alfabetu\n";
+	} else
+	{
+		std::cout << "Podałeś inny znak\n";
+	}
+
+	//wersja 3
+	if (characterFromUser >= 'a'
+		&& characterFromUser <= 'z')
+	{
+		std::cout << "Podałes małą literę alfabetu\n";
+	} else
+	{
+		std::cout << "Podałeś inny znak\n";
+	}
+}
+
+//Napisz program, który poprosi cie o twoje imię i cię przywita.
+void task3()
+{
+	std::string userName;
+	std::cout << "Podaj swoje imię\n";
+	std::cin >> userName;
+
+	std::cout << "Witaj " << userName << " tutaj.\n";
+
+	userName = "To jest nowy tekst";
+}
+
+//Program sprawdzaj¹cy czy podane has³o jest poprawne 
+// (np. jeżli hasło jest "abc123", 
+// program powinien wyświetlić "hasło poprawne",
+//  jeśli jest inne, powinien wyświetlić "hasło niepoprawne").
+void task4()
+{
+	std::string password;
+	std::cout << "Podaj hasŁo\n";
+	std::cin >> password;
+
+	if (password == "abc123")
+	{
+		std::cout << "Hasło poprawne\n";
+	} else
+		std::cout << "Hasło niepoprawne\n";
+}
+
+//Napisz program, który wczyta łańcuch znaków i policzy ile jest małych liter 'a'.
+void task5()
+{
+	std::string textFromUser = "dfsdfg";
+	std::cout << "Podaj tekst\n";
+	std::cin >> textFromUser;
+
+	std::cout << "Podany tekst: " << textFromUser << "\n";
+	std::cout << "Pierwszy znak w tekście: " << textFromUser[0] << "\n";
+	std::cout << "Drugi znak w tekście: " << textFromUser[1] << "\n";
+	int length = textFromUser.length();
+	std::cout << "Długość łańcucha znaków: " << length << "\n";
+	std::cout << "Ostatni znak w tekście: " << textFromUser[length - 1] << "\n";
+
+	int counter = 0;
+	for (int i = 0; i < length; i++)
+	{
+		if (textFromUser[i] == 'a')
+			counter++;
+	}
+
+	std::cout << "Małych liter 'a' jest: " << counter << "\n";
+}
+
+// Napisz program, który bêdzie prosić o hasło.
+// Nie przepuści dalej dopóki nie zostanie ono podane prawidłowo.
+void task6()
+{
+	std::string password;
+	do
+	{
+		std::cout << "Podaj hasło\n";
+		std::cin >> password;
+	} while (password != "abc123");
+}
+
+//Napisz program, który pobiera od użytkownika ciąg znaków 
+// i wyświetla liczbę samogłosek i spółgłosek w tym ciągu.
+void task7()
+{
+	std::string textFromUser;
+	std::cout << "Podaj tekst aby sprawdziæ w nim ilość spółgłosek i samogłosek: \n";
+	std::cin >> textFromUser;
+	std::cout << "Podany tekst to: " << textFromUser << "\n";
+
+	int vowels = 0;
+	int consonants = 0;
+	int length = textFromUser.length();
+	for (int i = 0; i < length; i++)
+	{
+		if (textFromUser[i] == 'a' ||
+			textFromUser[i] == 'e' ||
+			textFromUser[i] == 'i' ||
+			textFromUser[i] == 'o' ||
+			textFromUser[i] == 'u' ||
+			textFromUser[i] == 'ó' ||
+			textFromUser[i] == 'y' ||
+			textFromUser[i] == 'A' ||
+			textFromUser[i] == 'E' ||
+			textFromUser[i] == 'I' ||
+			textFromUser[i] == 'O' ||
+			textFromUser[i] == 'U' ||
+			textFromUser[i] == 'Ó' ||
+			textFromUser[i] == 'Y')
+			vowels++;
+		else
+			consonants++;
+	}
+
+	std::cout << "Samogłosek jest: " << vowels << "\n";
+	std::cout << "Spółgłosek jest: " << consonants << "\n";
+}
+
+/*
+* Poproś użytkownika o wprowadzenie liczby całkowitej w systemie dziesiętnym. Nastêpnie skonwertuj tę liczbę na system dwójkowy (binarny) i wyœwietl wynik.
+* Program sprawdzający czy podany ciąg znaków jest palindromem (czyli takim, który czytany od tyłu jest taki sam, jak czytany od przodu, np. "kajak")
+* Program sprawdzający czy podane dwa słowa są anagramami (czyli takimi, które zawierają te same litery, ale w innym układzie, np. "klasa" i "salka")
+* Program wyciągający informacje z numeru PESEL
+* Program implementujący algorytm szyfrowania Cezara (proste szyfrowanie, w którym każdy znak w tekście jest zastępowany innym znakiem, przesuniętym o stałą liczbę pozycji w alfabecie).*/
+
+
+//*Program sprawdzający czy podany ciąg znaków jest palindromem(czyli takim, który czytany od tyłu jest taki sam, jak czytany od przodu, np. "kajak")
+void task8() 
+{
+	std::string textFromUser;
+	std::cout << "Podaj tekst aby sprawdzić czy jest palindromem\n";
+	std::cin >> textFromUser;
+
+	std::string reverseText = "";
+	for (int i = textFromUser.length() - 1; i >= 0; i--)
+	{
+		reverseText = reverseText + textFromUser[i];
+	}
+
+	/*for (int i = 0; i < textFromUser.length(); i++)
+	{
+		reverseText = textFromUser[i] + reverseText;
+	}*/
+
+	if (textFromUser == reverseText)
+		std::cout << "Ten tekst jest palindromem\n";
+	else
+		std::cout << "Ten tekst nie jest palindromem\n";
+}
+//* Poproś użytkownika o wprowadzenie liczby całkowitej w systemie dziesiętnym. Następnie skonwertuj tę liczbę na system dwójkowy (binarny) i wyświetl wynik.
+void task9() 
+{
+	int number;
+	std::cout << "Podaj liczbę: \n";
+	std::cin >> number;
+
+	int tmpNumber = number;
+	std::string binNumber = "";
+
+	do
+	{
+		int rest = tmpNumber % 2;
+		tmpNumber = tmpNumber / 2;
+		if (rest == 0)
+			binNumber = "0" + binNumber;
+		else
+			binNumber = "1" + binNumber;
+	} while (tmpNumber != 0);
+
+	std::cout << "Liczba " << number << " po konwersji na system binarny " << binNumber;
+}
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+	//task1();
+	//task2();
+	//task3();
+	//task4();
+	//task5();
+	//task6();
+	//task7();
+	task8();
+}
