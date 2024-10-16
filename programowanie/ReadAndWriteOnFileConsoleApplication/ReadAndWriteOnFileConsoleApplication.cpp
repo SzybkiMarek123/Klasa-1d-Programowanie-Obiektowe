49cb3da70124f68aa75a25a3f7f2b8117b6fb6c5
q <q>
2024-10-16 08:50:32 +0200

    aaa

--- programowanie/ReadAndWriteOnFileConsoleApplication/ReadAndWriteOnFileConsoleApplication.cpp
+++ programowanie/ReadAndWriteOnFileConsoleApplication/ReadAndWriteOnFileConsoleApplication.cpp
@@ -1,20 +1,62 @@
-// ReadAndWriteOnFileConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <fstream>
+
+void task1()
+{
+    std::string name;
+    int age;
+
+    std::cout << "Podaj imie:\n";
+    std::cin >> name;
+
+    std::cout << "Podaj swój wiek:\n";
+    std::cin >> age;
+
+    std::ofstream writeInFile;
+    writeInFile.open("c:\\dane.txt", std::ios::app);
+
+    if (writeInFile.is_open() == true)
+    {
+        writeInFile << name;
+        writeInFile << "\n";
+        writeInFile.flush();
+        writeInFile << age << "\n";
+
+        writeInFile.close();
+    }
+    else
+        std::cout << "B³¹d otwarcia pliku\n";
+}
+
+//Odczyt zpliku tekstowego
+void task2()
+{
+    std::string name;
+    int age;
+    std::ifstream readFromFile;
+    readFromFile.open("c:\\dane.txt");
+    if (readFromFile.is_open())
+    {
+        while (readFromFile.eof() == false)
+        {
+            readFromFile >> name;
+            readFromFile >> age;
+
+            std::cout << "Imie: " << name << " ,wiek: " << age << "\n";
+            readFromFile.close();
+        }
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_CTYPE, "polish");
+    task2();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/*
+Dane s¹ buforowane, kiedy bufor siê przepe³ni wysy³a dane do pliku.
+Metoda flush wymusza opró?nienie bufora.
+Metoda close wywo³uje metode flush.
+*/
