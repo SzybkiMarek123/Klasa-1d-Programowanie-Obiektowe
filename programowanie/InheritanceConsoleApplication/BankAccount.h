26b611eccb680c43b61e4a5d8e20c074490e7eeb
q <q>
2024-11-27 08:53:48 +0100

    asdasd

--- programowanie/InheritanceConsoleApplication/BankAccount.h
+++ programowanie/InheritanceConsoleApplication/BankAccount.h
@@ -3,27 +3,23 @@
 
 class BankAccount
 {
-	//private oznacza, że te rzeczy są dostępne tylko w klasie. Pola powinny byæ prywatne.
-private:
-	double balance; // stan konta
+protected:
+	double balance; // saldo
 	std::string owner; //w³aœciciel
-	std::string currency; // waluta
+	std::string currency; //waluta
+
+	void SetBalance(double b);
 
 public:
 
-	//konstruktory publiczne
 	BankAccount();
 	BankAccount(double b, std::string o, std::string c);
-
-	void SetBalance(double b);
 
 	double GetBalance();
 
 	void AccountInformation();
 
 	void DepositToAccount(double amount);
-
-	bool WidthDrawFromAccount(double amount);
-
-	void TransferBetweenAccounts(BankAccount& targetAccount, double amount);
+	bool WidthdrawalFromAccount(double amount);
+	void TransferBetweenAcounts(BankAccount& targetAccount, double amount);
 };
