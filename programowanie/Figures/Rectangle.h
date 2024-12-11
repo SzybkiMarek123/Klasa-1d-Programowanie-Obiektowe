a38b536ce19d7c40e2023f8027137e9f3db06568
q <q>
2024-12-11 08:37:24 +0100

    aaaa

---
+++ programowanie/Figures/Rectangle.h
@@ -0,0 +1,12 @@
+
+#pragma once
+#include <iostream>
+#include "Quadrangle.h"
+
+class Rectangle : public Quadrangle
+{
+public:
+	Rectangle(double a, double b, std::string n);
+
+	double GetArea();
+};
---
+++ programowanie/Figures/Trapeze.cpp
@@ -0,0 +1,13 @@
+
+#include "Trapeze.h"
+
+Trapeze::Trapeze(double a, double b, double c, double d, double h, std::string n)
+    : Quadrangle(a, b, c, d, n)
+{
+    height = h;
+}
+
+double Trapeze::GetArea()
+{
+    return (sideB + sideD) * height / 2;
+}
