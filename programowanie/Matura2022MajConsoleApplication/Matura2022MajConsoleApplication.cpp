a919f7cdec7f7f01e388f9fab7a4453c185cfff1
q <q>
2025-03-31 13:29:34 +0200

    aaaaaa

--- programowanie/Matura2022MajConsoleApplication/Matura2022MajConsoleApplication.cpp
+++ programowanie/Matura2022MajConsoleApplication/Matura2022MajConsoleApplication.cpp
@@ -1,50 +1,39 @@
-// Matura2022MajConsoleApplication.cpp :
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <string>
 
-/*
-1. Kopiujemy pliki txt do folderu z projektem
-2. PPM na pliku txt i wybieramy Properties.
-3. Zmieniamy Item type na "Copy to"
-*/
+bool isPrime(int number)
+{
+	if (number < 2)
+		return false;
+
+	for (int i = 2; i <= sqrt(number); i++)
+	{
+		if (number % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int reversedNumber(int number)
+{
+	int reversed = 0;
+	while (number > 0)
+	{
+		reversed = reversed * 10 + number % 10;
+		number = number / 10;
+	}
+
+	return reversed;
+}
 
 int main()
 {
+	//Zadanie 1
+	setlocale(LC_CTYPE, "Polish");
 	std::ifstream file("przyklad.txt");
-
-	/*
-	int numbers[200];
-
-	for (int i = 0; i < 200; i++)
-	{
-		file >> numbers[i];
-	}*/
-
-	std::vector<int> numbers;
-
-	int n;
-	while (file >> n)
-	{
-		numbers.push_back(n);
-	}
-
-	/*
-	std::cout << "Odczytane liczby:\n";
-	for (int i = 0; i < numbers.size(); i++)
-	{
-		std::cout << numbers[i] << ", ";
-	}
-	std::cout << "\n";
-	*/
-
-	std::cout << "Odczytane liczby:\n";
-	for (int num : numbers)
-	{
-		std::cout << num << ", ";
-	}
-	std::cout << "\n";
 	std::vector<int> numbers;
 	int number;
 
@@ -53,24 +42,69 @@ int main()
 		numbers.push_back(number);
 	}
 
-	int counter = 0;
-	int first = 0;
-	for (int number : numbers)
+	for (int num : numbers)
 	{
-		std::string str = std::to_string(number);
-
-		if (str.length() > 0)
+		if (reversedNumber(num) % 17 == 0)
 		{
-			if (str.front() == str.back())
-			{
-				if (first == 0)
-				{
-					first = std::stoi(str);
-				}
-				counter++;
-			}
+			std::cout << reversedNumber(num) << "\n";
 		}
 	}
 
-	std::cout << counter << " " << first;
+	//Zadanie 2
+	int maxDifference = 0;
+	int highest = 0;
+	int reversedNum;
+
+	for (int num : numbers)
+	{
+		reversedNum = reversedNumber(num);
+		int difference = abs(num - reversedNum);
+
+		if (difference > maxDifference)
+		{
+			maxDifference = difference;
+			highest = num;
+		}
+	}
+
+	std::cout << highest << " " << maxDifference << "\n";
+
+	//Zadanie 3
+	for (int num : numbers)
+	{
+		int reversedNum = reversedNumber(num);
+		if (isPrime(num) && isPrime(reversedNum))
+		{
+			std::cout << num << "\n";
+		}
+	}
+
+	//Zadanie 4
+	std::vector<int> uniqueNumbers;
+	std::vector<int> occurrences;
+
+	for (int i = 0; i < numbers.size(); i++) {
+		bool found = false;
+		for (int j = 0; j < uniqueNumbers.size(); j++) {
+			if (uniqueNumbers[j] == numbers[i]) {
+				occurrences[j]++;
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			uniqueNumbers.push_back(numbers[i]);
+			occurrences.push_back(1);
+		}
+	}
+
+	int uniqueCount = uniqueNumbers.size();
+	int countTwice = 0, countThrice = 0;
+
+	for (int i = 0; i < occurrences.size(); i++) {
+		if (occurrences[i] == 2) countTwice++;
+		if (occurrences[i] == 3) countThrice++;
+	}
+
+	std::cout << uniqueCount << " " << countTwice << " " << countThrice << "\n";
 }
