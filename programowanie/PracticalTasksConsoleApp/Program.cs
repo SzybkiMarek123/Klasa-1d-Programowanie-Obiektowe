20df56f2fac346eee0dfd5906ed24251bad02c58
Sala213 <Sala213@ezse.pl>
2025-10-16 17:39:13 +0200

    asdasd

---
+++ programowanie/PracticalTasksConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿using PracticalTasksConsoleApp.Styczeń2023_1;
+
+task1 task1 = new task1();
+task1.Task();
---
+++ programowanie/PracticalTasksConsoleApp/Styczeń2023_1/task1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTasksConsoleApp.Styczeń2023_1
+{
+    internal class task1
+    {
+        public void Task() 
+        { 
+        
+        }
+    }
+}
