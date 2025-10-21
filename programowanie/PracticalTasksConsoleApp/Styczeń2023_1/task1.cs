eb9558f5de1fc8610d3592ddfcd2d539a63d4da8
Sala213 <Sala213@ezse.pl>
2025-10-21 15:08:41 +0200

    vbhu

--- programowanie/PracticalTasksConsoleApp/Styczeń2023_1/task1.cs
+++ programowanie/PracticalTasksConsoleApp/Styczeń2023_1/task1.cs
@@ -8,9 +8,37 @@ namespace PracticalTasksConsoleApp.Styczeń2023_1
 {
     internal class task1
     {
+        /**********************************************
+            nazwa funkcji: Task
+            opis funkcji: Oblicza największy wspólny dzielnik (NWD).
+            parametry: Brak.
+            zwracany typ i opis:Int, Funkcja wypisuje wynik w konsoli.
+            autor: Mateusz Papucki
+           ***********************************************/
         public void Task() 
-        { 
-        
+        {
+            int a, b;
+            string aa, bb;
+            Console.WriteLine("Podaj pierwszą liczbę");
+            aa = Console.ReadLine();    
+
+            Console.WriteLine("Podaj drugą liczbę");
+            bb = Console.ReadLine();
+
+            a = int.Parse(aa);
+            b = int.Parse(bb);
+
+            while (a != b) {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else { 
+                    b = b - a;
+                }
+
+            }
+            Console.WriteLine(a);
         }
     }
 }
