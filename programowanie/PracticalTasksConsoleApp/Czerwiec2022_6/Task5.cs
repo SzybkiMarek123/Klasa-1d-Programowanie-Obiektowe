d696fcc9d516df97b35a2b9eb854e35e6b6b2a8c
Sala213 <Sala213@ezse.pl>
2025-11-12 13:02:24 +0100

    sdasdas

--- programowanie/PracticalTasksConsoleApp/Czerwiec2022_6/Task5.cs
+++ programowanie/PracticalTasksConsoleApp/Czerwiec2022_6/Task5.cs
@@ -4,32 +4,37 @@
     {
         public void Task()
         {
-            Console.WriteLine($"Liczba zarejestrowanych osób to {Osoba.liczba}\n");
-            Osoba firstPerson = new Osoba();
-            Console.WriteLine("Wypisanie imienia z pierwszego obiektu:");
-            firstPerson.Witaj("Mateusz");
-
+            Console.WriteLine($"Liczba zarejestrowanych osób: {Osoba.liczba}\n");
+            Osoba pierwszaOsoba = new Osoba();
+            Console.WriteLine("Wypisanie pierwszego imienia:");
+            pierwszaOsoba.Witaj("Mateusz");
             Console.WriteLine("\n");
 
-            Console.WriteLine("Wprowadź dane do stworzenia drugiego obiektu:");
+
+            Console.WriteLine("Wprowadź dane do stworzenia drugiego imienia:");
             Console.WriteLine("Wprowadź imię:");
             string name = Console.ReadLine();
+
 
             Console.WriteLine("Wprowadź id:");
             int id = int.Parse(Console.ReadLine());
 
-            Osoba secondPerson = new Osoba(name, id);
+
+            Osoba drugaOsoba = new Osoba(name, id);
             Console.WriteLine("\n");
-            Console.WriteLine("Wypisanie imienia z drugiego obiektu:");
-            secondPerson.Witaj("Marek");
+            Console.WriteLine("Wypisanie drugiego imienia:");
+            drugaOsoba.Witaj("Marek");
 
-            Osoba thirdPerson = new Osoba(secondPerson);
-            Console.WriteLine("\nWypisanie imienia z trzeciego obiektu:");
-            thirdPerson.Witaj("Mirek");
+
+            Osoba trzeciaOsoba = new Osoba(drugaOsoba);
+            Console.WriteLine("\n");
+            Console.WriteLine("Wypisanie trzeciego imienia:");
+            trzeciaOsoba.Witaj("Mirek");
 
             Console.WriteLine("\n");
 
-            Console.WriteLine($"Liczba zarejestrowanych osób to {Osoba.liczba}");
+
+            Console.WriteLine($"Liczba zarejestrowanych osób: {Osoba.liczba}");
         }
     }
 }
